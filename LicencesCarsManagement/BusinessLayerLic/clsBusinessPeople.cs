dfdc923c3437264ed50210f2f597334c3e60805d
Abdulrahman omar <an.hablil117@gmail.com>
2026-04-29 16:17:49 +0300

    add comment on businwalayer

--- LicencesCarsManagement/BusinessLayerLic/clsBusinessPeople.cs
+++ LicencesCarsManagement/BusinessLayerLic/clsBusinessPeople.cs
@@ -12,7 +12,7 @@ namespace BusinessLayerLic
     public class clsBusinessPeople
     {
         enum enMode { enAdd ,enUpdate};
-        // call
+        // oh 
         enMode Mode = enMode.enAdd; 
 
         public int PersonID { get; set; }
